94af66be1b38d0eaa03ede86da2375d16a79cb1a
LEGGETT FRANK <frank.leggett@etu.univ-poitiers.fr>
2024-02-28 17:55:20 +0100

    Ajout tests pour PileBacktracking

---
+++ src/PileBacktrackingTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PileBacktrackingTest {
+    @Test
+    public void test_pushPile(){} //No need, calls Stack method push()
+    @Test
+    public void test_pullPile(){}//No need, calls Stack method pop()
+    @Test
+    public void test_getTopPile(){}//No need, calls Stack method peek()
+    @Test
+    public void test_getSizePile(){}//No need, calls Stack method size()
+    @Test
+    public void test_isPileEmpty(){}//No need, calls Stack method isEmpty()
+}
