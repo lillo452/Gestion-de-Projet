fca1d3b480387e1fe71ddf6cad41c97ac0b71d69
LEGGETT FRANK <frank.leggett@etu.univ-poitiers.fr>
2024-03-05 08:44:28 +0100

    tests class Variable

---
+++ VariableTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+class VariableTest{
+
+    @Test
+    public void test_f_getStack(){} //No need returns stack
+    @Test
+    public void test_f_addFst(){} //No need calls Stack method add()
+    @Test
+    public void test_f_getFst(){} //No need calls Stack method pop()
+}
