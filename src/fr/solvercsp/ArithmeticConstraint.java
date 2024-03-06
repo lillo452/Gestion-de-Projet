cf0744774208128b629295e43450bdb6f5825aaa
Lillo Gavois <43553403+lillo452@users.noreply.github.com>
2024-03-06 09:10:51 +0100

    Create ArithmeticConstraint.java

---
+++ src/fr/solvercsp/ArithmeticConstraint.java
@@ -0,0 +1,17 @@
+public class ArithmeticConstraint extends Constraint {
+
+    private Variable variable;
+    private String arithmeticOperator;
+    private int constant;
+
+    public ArithmeticConstraint(Variable variable, String arithmeticOperator, int constant) {
+        this.variable = variable;
+        this.arithmeticOperator = arithmeticOperator;
+        this.constant = constant;
+    }
+
+    @Override
+    public boolean isSatisfied() {
+    //Sous-tache 3.1 Antony
+    }
+}
