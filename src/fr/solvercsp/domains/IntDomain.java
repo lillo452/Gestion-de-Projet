e8e4f37f5049ff91fccdb27abc21d8b8ec98aea1
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-03-12 14:40:52 +0100

    Add methods to IntDomain + remove excludedValues

--- src/fr/solvercsp/domains/IntDomain.java
+++ src/fr/solvercsp/domains/IntDomain.java
@@ -10,18 +10,49 @@ public class IntDomain implements Domain{
 
     private int min;
     private int max;
-    private List<Integer> excludedValues;
 
     /**
      * Constructor for the IntDomain class.
      * @param min The minimum value in the domain.
      * @param max The maximum value in the domain.
-     * @param excludedValues A list of values to be excluded from the domain.
      */
-    public IntDomain(int min, int max, List<Integer> excludedValues){
+    public IntDomain(int min, int max){
         this.min = min;
         this.max = max;
-        this.excludedValues = excludedValues;
+    }
+
+    /**
+     * Returns if the domain is empty. An empty domain is a domain where the minimum value is greater than the maximum value.
+     * @return True if the domain is empty, false otherwise.
+     */
+    public boolean isEmpty(){
+        return min > max;
+    }
+
+    /**
+     * Returns if the value is in the domain.
+     * @param i The value to check.
+     * @return True if the value is in the domain, false otherwise.
+     */
+    public boolean isIn(int i){
+        return i >= min && i <= max;
+    }
+
+    /**
+     * Returns if the domain is bound by the given values.
+     * Only uses for tests
+     * @return True if the domain is bound by the given values, false otherwise.
+     */
+    public boolean isBoundBy(int min, int max){
+        return this.min == min && this.max == max;
+    }
+
+    /**
+     * Returns if the domain is a single value domain.
+     * @return True if the domain is a single value domain, false otherwise.
+     */
+    public boolean isSingle(){
+        return min == max;
     }
 
     /**
